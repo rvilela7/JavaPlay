c9e42182b867dd36b093123f7457186e103c5202
Rui Vilela <ruivil@gmail.com>
2022-04-20 17:50:36 +0100

    First Commit

---
+++ Integers.java
@@ -0,0 +1,13 @@
+// Your First Program
+
+class Integers {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 9;
+        
+        int c = a + b;
+        System.out.println(c);
+    }
+}
+
+
